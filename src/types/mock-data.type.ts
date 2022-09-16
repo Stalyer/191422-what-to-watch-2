506d77222b23225671db8c0f5e5c92a0886c4f70
Ivan Zharkov <stalyer@yandex.ru>
2022-09-16 22:17:43 +0700

    Удалил закомментированный код

--- src/types/mock-data.type.ts
+++ src/types/mock-data.type.ts
@@ -1,14 +1,11 @@
 export type MockData = {
   name: string[];
   description: string[];
-  // publictionDate:
   released: number[];
   previewVideoImage: string[];
   previewVideoLink: string[];
   starring: string[];
   director: string[];
-  // runTime: number;
-  // commentCount: number;
   posterImage: string[];
   backgroundImage: string[];
   backgroundColor: string[];
